4a559a055b1a7d9ee9e5ae1deec983343d870a2c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-12-18 20:39:26 -0600

    agregando el script de swiper con paginacion y navegacion

---
+++ app.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
